03cae1a88f6b1b4ec99b1f3f3792a16edda47dce
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-19 00:03:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,6 @@
 class Artist
   attr_accessor :name
+  attr_reader :songs
 
   def initialize(name)
     @name = name
@@ -7,13 +8,14 @@ class Artist
   end
 
   def add_song(song)
+    @songs << song
     song.artist = self
   end
 
   def songs
-    Song.all.select {|song| song.artist == self}
+      Song.all.select {|song| song.artist == self}
   end
-
+  
   def add_song_by_name(name)
     song = Song.new(name)
     add_song(song)
